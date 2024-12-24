c13ac40e3bd541bac0b63f8814b5f9f444a21fec
raja1928 <53326027+raja1928@users.noreply.github.com>
2024-12-24 13:30:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ resource "aws_eks_cluster" "example" {
   }
 }
 
+sh 'terraform state rm aws_iam_role.eks_cluster_role'
+
 data "aws_iam_roles" "existing_roles" {
   filter {
     name = "roleName"

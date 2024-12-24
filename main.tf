ccaef7b21fc7d2949b590ef99d2c4a003810ee6f
raja1928 <53326027+raja1928@users.noreply.github.com>
2024-12-24 13:59:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,19 +14,12 @@ resource "aws_subnet" "eks_subnets" {
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
 
-resource "aws_eks_cluster" "example" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role[count.index].arn  # Use count.index to reference the specific instance
-  # Other configurations...
-}
-
 data "aws_iam_role" "existing_role" {
-  name = "eks-cluster-role"  # Correct argument name
+  name = "eks-cluster-role"  # Name of the IAM role you want to check
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
-  count = length(data.aws_iam_role.existing_role.id) == 0 ? 1 : 0
-
+  count              = length(data.aws_iam_role.existing_role.id) == 0 ? 1 : 0  # Only create if the role doesn't exist
   name               = "eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -40,6 +33,13 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
+resource "aws_eks_cluster" "example" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role[0].arn  # Reference the created role
+
+  # Other configurations for EKS cluster...
+}
+
 resource "aws_security_group" "eks_sg" {
   name        = "eks_security_group"
   description = "EKS cluster security group"
@@ -47,17 +47,17 @@ resource "aws_security_group" "eks_sg" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role[count.index].name  # Use count.index here too
+  role       = aws_iam_role.eks_cluster_role[0].name  # Attach policies to the role
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_vpc_policy" {
-  role       = aws_iam_role.eks_cluster_role[count.index].name  # Use count.index here too
+  role       = aws_iam_role.eks_cluster_role[0].name  # Attach policies to the role
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_VPC_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
-  role       = aws_iam_role.eks_cluster_role[count.index].name  # Use count.index here too
+  role       = aws_iam_role.eks_cluster_role[0].name  # Attach policies to the role
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
